26d3c23f97a34d1248a19b27a69b3376d40ebb8c
Calvin-Spens <calvinsjohnson.com@gmail.com>
2023-08-23 08:17:46 -0700

    Starting tkinter implementation

--- lottery.py
+++ lottery.py
@@ -1,11 +1,11 @@
-import random
+from tkinter import *
+from tkinter import ttk
 from random import choices 
 
 def main():
     print('How many lottery contestants:')
     lott_num = int(input())
-    input_list = input('Enter the team names in order separated by a space: \n')
-    print("\n")
+    input_list = input('Enter the team names, lowest odds first: \n')
     team_list = input_list.split()
     team_dct = {(i+1): team_list[i] for i in range(len(team_list))}
     print(team_dct)
@@ -13,15 +13,18 @@ def main():
 
 def lottery_choices(lott_num, team_dct):
     lott_amount = [(2**i)*3 for i in range(lott_num)]
-    print(lott_amount)
     teams = [ i + 1 for i in range(lott_num)]
     for i in range(lott_num):
-        print(i)
         prob = [count/sum(lott_amount) for count in lott_amount]
-        print(prob)
         pick = int(choices(teams, prob)[0])
-        print(pick)
         print(f"Pick number {i+1} is Team {team_dct[pick]}")
         lott_amount[pick-1] = 0 
 
 main()
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
